dda894171c08867d0d40c1a7353d5ac734807e91
John Tyner <jtyner@gmail.com>
2023-08-15 13:07:55 -0400

    add code to load credentials

--- src/credentials.rs
+++ src/credentials.rs
@@ -6,6 +6,10 @@ const SAPI_ID: &str = "SECRET_SIGNING_KEY";
 const SRSA_ID: &str = "SECRET_CRYPTO_ACCESS_KEY";
 const HOST_ID: &str = "SERVER";
 
+const ENV_PREFIX: &str = "UBIQ_";
+
+const SERVER: &str = "api.ubiqsecurity.com";
+
 pub struct Credentials {
     params: std::collections::HashMap<String, String>,
 }
@@ -44,25 +48,88 @@ impl Credentials {
         };
     }
 
+    fn from_env() -> Credentials {
+        let mut c = Self::construct();
+
+        for s in [PAPI_ID, SAPI_ID, SRSA_ID, HOST_ID] {
+            let k = format!("{}{}", ENV_PREFIX, s);
+
+            match std::env::var_os(k.clone()) {
+                None => (),
+                Some(v) => match v.to_str() {
+                    None => (),
+                    Some(val) => {
+                        c.params.insert(s.to_string(), val.to_string());
+                    }
+                },
+            }
+        }
+
+        c
+    }
+
+    fn from_file(path: &String, prof: &String) -> Result<Credentials> {
+        let mut c = Self::construct();
+        let mut ini = configparser::ini::Ini::new_cs();
+
+        // todo separate loading from file and parsing so that
+        // parsing can be unit tested more easily
+        match ini.load(path.as_str()) {
+            Err(ref s) => return Err(Error::from_string(s.clone())),
+            Ok(profs) => match profs.get(prof) {
+                None => {
+                    return Err(Error::from_str("specified profile not found"))
+                }
+                Some(creds) => {
+                    for k in [PAPI_ID, SAPI_ID, SRSA_ID, HOST_ID] {
+                        let v = creds.get(k);
+                        /*
+                         * this is kinda nasty:
+                         * creds is a HashMap<String, Option<String>>,
+                         * so the creds.get() returns an Option<Option<String>>
+                         *
+                         * the first option tells us whether k even exists
+                         * if it does, then the value could be None or a string,
+                         * hence the second Option
+                         *
+                         * i suppose it could be a parse error if the second
+                         * Option is None, but that's a problem for another
+                         * time. for now, ignore it.
+                         */
+                        if v.is_some() && v.unwrap().is_some() {
+                            c.params.insert(
+                                k.to_string(),
+                                v.unwrap().as_ref().unwrap().to_string(),
+                            );
+                        }
+                    }
+                }
+            },
+        }
+
+        Ok(c)
+    }
+
     pub fn new(
         opt_path: Option<String>,
         opt_prof: Option<String>,
     ) -> Result<Credentials> {
-        let mut ini = configparser::ini::Ini::new_cs();
+        let path_spec: bool = opt_path.is_some();
 
         let path: String;
         let prof: String;
-        let conf: &std::collections::HashMap<
-            String,
-            std::collections::HashMap<String, Option<String>>,
-        >;
 
-        match opt_path {
-            Some(p) => path = p,
-            None => match Self::get_default_filepath() {
+        let res: Result<Credentials>;
+
+        let mut creds: Credentials;
+
+        if path_spec {
+            path = opt_path.unwrap();
+        } else {
+            match Self::get_default_filepath() {
                 Ok(p) => path = p,
                 Err(e) => return Err(e),
-            },
+            }
         }
 
         match opt_prof {
@@ -70,26 +137,48 @@ impl Credentials {
             None => prof = "default".to_string(),
         }
 
-        match ini.load(path.as_str()) {
-            Err(ref s) => return Err(Error::from_string(s.clone())),
-            Ok(ref map) => conf = map,
+        creds = Self::from_env();
+
+        res = Self::from_file(&path, &prof);
+        if res.is_ok() {
+            let mut t_creds = res.unwrap();
+
+            if path_spec {
+                std::mem::swap(&mut creds, &mut t_creds);
+                creds.merge(t_creds, &[HOST_ID]);
+            } else {
+                creds.merge(t_creds, &[PAPI_ID, SAPI_ID, SRSA_ID, HOST_ID]);
+            }
+        } else if path_spec {
+            return res;
         }
 
-        Err(Error::from_str("not implemented"))
+        for k in [PAPI_ID, SAPI_ID, SRSA_ID] {
+            if creds.params.get(&k.to_string()).is_none() {
+                return Err(Error::from_str("invalid/incomplete credentials"));
+            }
+        }
+
+        Self::create(
+            creds.params.remove(&PAPI_ID.to_string()).unwrap(),
+            creds.params.remove(&SAPI_ID.to_string()).unwrap(),
+            creds.params.remove(&SRSA_ID.to_string()).unwrap(),
+            creds.params.remove(&HOST_ID.to_string()),
+        )
     }
 
     pub fn create(
         papi: String,
         sapi: String,
         srsa: String,
-        opt_host: Option<&String>,
+        opt_host: Option<String>,
     ) -> Result<Credentials> {
         let mut c = Self::construct();
         let mut host: String;
 
         match opt_host {
-            None => host = "api.ubiqsecurity.com".to_string(),
-            Some(h) => host = h.clone(),
+            None => host = SERVER.to_string(),
+            Some(h) => host = h,
         }
 
         if !host.starts_with("http://") && !host.starts_with("https://") {
@@ -104,19 +193,41 @@ impl Credentials {
         Ok(c)
     }
 
-    fn papi(&self) -> Option<&String> {
-        return self.params.get(PAPI_ID);
+    fn merge(&mut self, other: Credentials, which: &[&str]) {
+        for w in which {
+            let k = w.to_string();
+            let sp = other.params.get(&k);
+
+            if self.params.get(&k).is_none() && sp.is_some() {
+                self.params.insert(k, sp.unwrap().clone());
+            }
+        }
     }
 
-    fn sapi(&self) -> Option<&String> {
-        return self.params.get(SAPI_ID);
+    fn papi(&self) -> &String {
+        return self.params.get(PAPI_ID).unwrap();
     }
 
-    fn srsa(&self) -> Option<&String> {
-        return self.params.get(SRSA_ID);
+    fn sapi(&self) -> &String {
+        return self.params.get(SAPI_ID).unwrap();
     }
 
-    fn host(&self) -> Option<&String> {
-        return self.params.get(HOST_ID);
+    fn srsa(&self) -> &String {
+        return self.params.get(SRSA_ID).unwrap();
+    }
+
+    fn host(&self) -> &String {
+        return self.params.get(HOST_ID).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Credentials;
+
+    #[test]
+    fn has_default_filepath() {
+        let r = Credentials::get_default_filepath();
+        assert!(r.is_ok(), "{}", r.unwrap_err().to_string())
     }
 }
