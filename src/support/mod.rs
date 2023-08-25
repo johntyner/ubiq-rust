9db062cf3180990caad6973f04eaac4fd6ca76ed
John Tyner <jtyner@gmail.com>
2023-08-25 13:16:21 -0400

    use rustcrypto to unwrap private key

--- src/support/mod.rs
+++ src/support/mod.rs
@@ -5,34 +5,16 @@ pub mod cipher;
 pub mod decryption;
 pub mod encryption;
 
-use rsa::pkcs8::DecodePrivateKey;
 use rand::RngCore;
+use rsa::pkcs8::DecodePrivateKey;
 
 pub fn getrandom(buf: &mut [u8]) -> Result<()> {
     Ok(rand::thread_rng().fill_bytes(buf))
 }
 
 pub fn unwrap_data_key(wdk: &[u8], epk: &str, srsa: &str) -> Result<Vec<u8>> {
-    let mut raw: Vec<u8> = Vec::new();
-
-    if false {
-        let pk = rsa::RsaPrivateKey::from_pkcs8_encrypted_pem(
-            epk, srsa.as_bytes())?;
-
-        raw = pk.decrypt(rsa::oaep::Oaep::new::<sha1::Sha1>(), wdk)?;
-    } else {
-
-        let pk = openssl::pkey::PKey::private_key_from_pem_passphrase(
-            epk.as_bytes(),
-            srsa.as_bytes(),
-        )?;
-
-        let mut pk_ctx = openssl::pkey_ctx::PkeyCtx::new(&pk)?;
-        pk_ctx.decrypt_init()?;
-        pk_ctx.set_rsa_oaep_md(&openssl::md::Md::sha1())?;
-        pk_ctx.set_rsa_padding(openssl::rsa::Padding::PKCS1_OAEP)?;
-        pk_ctx.decrypt_to_vec(wdk, &mut raw)?;
-    }
-
-    Ok(raw)
+    Ok(
+        rsa::RsaPrivateKey::from_pkcs8_encrypted_pem(epk, srsa.as_bytes())?
+            .decrypt(rsa::oaep::Oaep::new::<sha1::Sha1>(), wdk)?,
+    )
 }
